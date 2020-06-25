4ff242b60e08378f800bfc035e2b585badf505b5
ecoknows <66323903+ecoknows@users.noreply.github.com>
2020-06-25 17:33:52 +0800

    Pagination!!!
    Reversing the adding
    Adding Pagination

--- database/database.js
+++ database/database.js
@@ -13,6 +13,11 @@ const _2_check_tbl = 'check_table_2';
 const userId = 'Amber';
 const TAG = 'DATABASE ';
 
+let _1_currentOffset = -1;
+let _2_currentOffset = -1;
+
+const PAGING_LIMIT = "4";
+
 let isInitial = true;
 
 const InitialData =(dataState)=> {
@@ -178,7 +183,7 @@ const DropTable =()=>{
 }
 
 const _1_SelecData = (setStateData1,data1) =>{
-  let query = "SELECT * from " + _1_data;
+  let query = "SELECT * from " + _1_data + " ORDER BY id DESC LIMIT 5";
   let params = [];
 
   db.transaction(
@@ -186,7 +191,9 @@ const _1_SelecData = (setStateData1,data1) =>{
       tx.executeSql(query, params,(tx, results) =>{
         if(results.rows._array.length > 0){
           //console.log(results.rows._array);
-          for(let i = 0; i < results.rows._array.length; i++){
+          
+          _1_currentOffset = results.rows._array[results.rows._array.length-1].id;
+          for(let i = results.rows._array.length-1; i != -1 ; i--){
             _1_SelectCheckList(results.rows._array[i],setStateData1,data1);
           }
          //console.log(...results.rows._array);
@@ -202,7 +209,7 @@ const _1_SelecData = (setStateData1,data1) =>{
 }
 
 const _2_SelecData = (setStateData2,data2) =>{
-  let query = "SELECT * from " + _2_data;
+  let query = "SELECT * from " + _2_data + " ORDER BY id DESC LIMIT 5";
   let params = [];
 
 
@@ -211,7 +218,8 @@ const _2_SelecData = (setStateData2,data2) =>{
       tx.executeSql(query, params,(tx, results) =>{
         if(results.rows._array.length > 0){
           //console.log(results.rows._array);
-          for(let i = 0; i < results.rows._array.length; i++){
+          _2_currentOffset = results.rows._array[results.rows._array.length-1].id;
+          for(let i = results.rows._array.length-1; i != -1 ; i--){
             _2_SelectCheckList(results.rows._array[i],setStateData2,data2);
           }
          //console.log(...results.rows._array);
@@ -309,6 +317,60 @@ const SeeData =()=>{
   );
 
 }
+
+const _2_NextPage =(data)=>{
+  const { setStateData2, data2 } = data;
+  let query = ' ';
+  let params = [];
+  console.log('current ', _2_currentOffset);
+  query = _2_currentOffset == -1 ? "SELECT * from " + _2_data + " ORDER BY id DESC LIMIT " + PAGING_LIMIT  :
+          "SELECT * from " + _2_data + " WHERE id < "+ _2_currentOffset.toString() +" ORDER BY id DESC LIMIT " + PAGING_LIMIT;
+
+  db.transaction(
+    (tx)=> {
+      tx.executeSql(query, params,(tx, results) =>{
+        if(results.rows._array.length > 0){
+          //console.log(results.rows._array);
+          data2.push(results.rows._array);
+          setStateData2(items=> [...items, ...results.rows._array]);
+          _2_currentOffset = results.rows._array[results.rows._array.length-1].id;
+        }
+      }, function(tx,err) {
+        Alert.alert(err.message);
+        return;
+      })
+    }
+  );
+
+}
+
+const _1_NextPage =(data)=>{
+  const { setStateData1, data1 } = data;
+  let query = ' ';
+  let params = [];
+  console.log('current ', _1_currentOffset);
+  query = _1_currentOffset == -1 ? "SELECT * from " + _1_data + " ORDER BY id DESC LIMIT " + PAGING_LIMIT  :
+          "SELECT * from " + _1_data + " WHERE id < "+ _1_currentOffset.toString() +" ORDER BY id DESC LIMIT " + PAGING_LIMIT;
+
+  db.transaction(
+    (tx)=> {
+      tx.executeSql(query, params,(tx, results) =>{
+        if(results.rows._array.length > 0){
+         // console.log(results.rows._array);
+          data1.push(results.rows._array);
+          setStateData1(items=> [...items, ...results.rows._array]);
+          _1_currentOffset = results.rows._array[results.rows._array.length-1].id;
+        }
+      }, function(tx,err) {
+        Alert.alert(err.message);
+        return;
+      })
+    }
+  );
+
+}
+
+
 export {
     CheckNote,
     AddNote,
@@ -318,5 +380,7 @@ export {
     GetDataPos,
     InitialData,
     RemovePos,
-    SeeData
+    SeeData,
+    _1_NextPage,
+    _2_NextPage,
 }
